147a1b6c74779beef63c62844d0fcae3bee26192
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2020-10-12 15:22:06 +0200

    Update aoe.py

--- cogs/aoe.py
+++ cogs/aoe.py
@@ -14,10 +14,10 @@ import utils.utilities as ut
 class AoE(commands.Cog):
     """ Age of Empires 2 specific commands """
     
-    def __init__(self, bot, logger):  # bot & logger are from --> discord_bot.CommandBot
+    def __init__(self, bot):  # bot is from --> discord_bot.CommandBot
         self.bot = bot
-        logger = logging.getLogger("discord.AoE")
-        logger.info("AoE() started...")
+        self.logger = logging.getLogger("discord.AoE")
+        self.logger.info("AoE() started...")
 
     @commands.command(name="online",
                       help="Fetches the current amount of players in-game in AoE2:DE."
