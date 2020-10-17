9f7bb9ba3291ccd26a4b7abdd629a52be5df8910
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2020-10-17 13:32:35 +0200

    [new command] command to get the top X players of the 1v1 RM leaderboard of AoE2:DE
    - uses the already made plotting parts I used in my other repo
    - made this one fit in a command and all - it now also merges the images vertically together, so you can have one big giant image (looks sort of nice, actually) :D 
    - also implemented the general function to delete the created images afterwards from the utils.utilities file

--- cogs/aoe.py
+++ cogs/aoe.py
@@ -63,3 +63,117 @@ class AoE(commands.Cog):
         plt.savefig(filepath)
         logger.info(f"Saving plotted graph to {filepath}...")
         await ctx.send(file=discord.File(filepath))
+      
+    @commands.command(name="top",
+                      help="Displays graph(s) of the Top players in 1v1 RM of the AoE2: DE leaderboard."
+                           "\nSimply enter the number of players you want to see! Defaults to 5 if left empty."
+                           "\nExample usage:"
+                           "\n- !top 10")
+    async def top(self, ctx, amount: int = 5):
+        """
+        Command:\n
+        Displays graph(s) for the top players of the current AoE2:DE leaderboard.
+        For each 5 players, a new graph will be plotted.
+        At the end, all graphs, will be merged into one image vertically - with the help of a already pre-defined
+        function which merges X number of images.
+        After plotting the graphs and sending the merged image, all the used images will be swiftly deleted.
+        ToDo: make the command also usable with other AoE2:DE game modes, and not only 1v1 RM (should be pretty easy)
+
+        Might put the separate parts of the function into their own function parts.
+
+        :param ctx: the Context data (gets it from Discord)
+        :param amount: the amount of players to get from the leaderboard - defaults to the first 5 players
+
+        :return: sends the plotted graph
+        """
+
+        await ctx.send("Depending on how many players you requested, "
+                       "it might take a little while to plot the graphs. "
+                       "Please have a little bit of patience, thanks!")
+        url = f"https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start=1&count={amount}"
+        data = ut.get_request_response(url, json=True)
+        data = data["leaderboard"]
+
+        # saving in here the filepaths of the possibly to be merged files
+        # if the amount of players to extract is <= than 5, this list here will simply be ignored, as it is not needed
+        files_to_merge = []
+
+        # determines how many runs are needed to be completed with the given amount of players to get
+        runs = float(amount) / float(5)
+
+        start = 0
+        end = 5
+        from math import ceil
+        # here the loop runs as long as there are still players to extract
+        for _ in range(ceil(runs)):  # the math.ceil() method helps us rounding UP to the nearest integer
+
+            # always takes the next up to 5 players from the remaining data
+            chunk = data[start:end:1]
+
+            df = pd.DataFrame(chunk)
+
+            ax = df.plot(x="rank", y=["highest_rating", "rating", "games", "wins", "losses"], kind="bar")
+            ax.set_xlabel("Player", x=0.02, y=0.8, style="italic", bbox={"facecolor": "orange"})
+            ax.set_ylabel("Amount", x=1, y=0.05, style="italic", bbox={"facecolor": "orange"})
+            ax.set_title("Age Of Empires 2: Definitive Edition - Top Players (Leaderboard) 1v1 RM",
+                         fontweight="bold", x=0.5, y=1.075, bbox={"facecolor": "orange"})
+            # to set the legend to the place of your choosing
+            # in my case outside the plot/box area
+            ax.legend(bbox_to_anchor=(1, 1), loc="upper left")
+
+            # retrieve x labels (names of the players) and current rank in the leaderboard and set them for each x label
+            x_labels = []
+            for idx, player in enumerate(chunk):
+                player = str(chunk[idx]["rank"]) + " - " + chunk[idx]["name"]
+                x_labels.append(player)
+
+            # for the rotating of the x labels to horizontal and centering them
+            ax.set_xticklabels(x_labels, rotation=0, ha="center")
+
+            # to set the bar label for each bar in the chart with the actual value of the bar
+            for rect in ax.patches:
+                y_value = rect.get_height()
+                x_value = rect.get_x() + rect.get_width() / 2
+
+                space = 3
+                va = "bottom"
+
+                if y_value < 0:  # take values <0 also into consideration
+                    space *= -1
+                    va = "top"
+
+                label = "{:.0f}".format(y_value)
+                ax.annotate(
+                    label,
+                    (x_value, y_value),
+                    xytext=(0, space),
+                    textcoords="offset points",
+                    ha="center",
+                    va=va,
+                    rotation=45)
+
+            # sets the current figure via gcf() to a size in inches,
+            # so the graph can be saved in a higher resolution / bigger size (so it is more readable)
+            figure = plt.gcf()
+            figure.set_size_inches(19, 10)
+
+            filepath = f"..\\DiscordBot\\resources\\images\\top_players_{_}.png"
+            files_to_merge.append(filepath)
+            # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.savefig.html --> all the parameters usable
+            plt.savefig(filepath, bbox_inches="tight")
+
+            # continue with more data, if available
+            end += 5
+            start += 5
+
+        if amount > 5:  # since only 5 players per plot, save them into a list to later merge them together
+            merged_file = ut.merge_images(files_to_merge, file_name=f"top_{amount}_players")
+            await ctx.send(f"Requested data of the Top {amount} players of the current AoE2:DE leaderboard:",
+                           file=discord.File(merged_file))
+            # deleting all the image files with the naming scheme given - if none is found/deleted, nothing will happen
+            ut.del_image_files(directory="..\\DiscordBot\\resources\\images\\", patterns=("top_*.png", "top_*.jpg"))
+        else:  # if <= 5, simply only send the 1 plot
+            await ctx.send(f"Requested data of the Top {amount} players of the current AoE2:DE leaderboard:",
+                           file=discord.File(filepath))
+            # deleting all the image files with the naming scheme given - if none is found/deleted, nothing will happen
+            ut.del_image_files(directory="..\\DiscordBot\\resources\\images\\", patterns=("top_*.png", "top_*.jpg"))
